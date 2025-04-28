ffca0987bbf32d45a05328f8992d3bfaf0fcb0cd
jeff <jeffersonkline@gmail.com>
2025-04-28 16:55:32 -0700

    small changes

--- MET3.0/cytoplasm.py
+++ MET3.0/cytoplasm.py
@@ -12,15 +12,21 @@ class mitochondriaClass():
         self.matrix = matrixState()
 
     def exportATP(self):
-        if (self.matrix.ATP >= 1):
+        if (self.matrix.ATP >= 25):
             self.matrix.ATP -= 1
             self.cell.ATP += 1
 
     def run(self):
-        for i in range(50):
+        for i in range(500):
             self.matrix.ETC.Cycle(self.matrix)
             self.matrix.CAC.Cycle(self.matrix)
-            time.sleep(.01)
+            self.exportATP()
+            time.sleep(.1)
+
+
+class gylcolysis():
+    def __init__(self, cell):
+        self.cell = cell
 
 # Stores all molecules in cytoplasm
 class cellState():
@@ -28,11 +34,12 @@ class cellState():
     def __init__(self):
         self.mitochondria = mitochondriaClass(self)
 
-
         # Energy Molecules
         self.ADP = 100
         self.GTP = 100
         self.ATP = 100
+        self.NAD = 100
+        self.NADH = 100
 
         # Common Molecules
         self.O2 = 100
@@ -40,9 +47,11 @@ class cellState():
 
         # Glycolysis Molecules
         self.glucose = 100
+        self.Glucose_6_Phosphate = 100
+        self.Fructose_6_phosphate = 100
+        self.Fructose_1_6_biphosphate = 100
+        self.glyceraldehyde_3_phosphate = 100
         self.pyruvate = 100
-        self.NAD = 100
-        self.NADH = 100
 
 cellMain = cellState()
 cellMain.mitochondria.run()
